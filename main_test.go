92bc154db29641b094348b7e5015a422d6479499
Peter Vypov <peter.vypov@gmail.com>
2014-12-11 13:45:58 +0300

    Some refactoring

--- main_test.go
+++ main_test.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"go/build"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"path/filepath"
 	"reflect"
 	"runtime"
 	"strings"
 	"testing"
 )
 
-const (
-	hostPrefix    = "http://localhost/"
-	packagePrefix = "github.com/pietv/gopaths/"
+var (
+	hostPrefix    = "http://localhost:6118/"
+	packagePrefix = "github.com/pietv/gopaths/testdata"
+	dirPrefix     = func() string {
+		p, _ := build.Default.Import(packagePrefix, "", build.FindOnly)
+		return p.Dir
+	}()
+
+	sep = string(os.PathSeparator)
 )
 
 func init() {
@@ -22,25 +30,142 @@ func init() {
 	log.SetOutput(ioutil.Discard)
 }
 
-// slice trims extra '\n' from the HTTP response and then splits.
+// slice trims extra '\n' from the HTTP response and then splits it.
 func slice(resp string) []string {
 	return strings.Split(strings.Trim(resp, "\n"), "\n")
+}
+
+// prefixDir appends the prefix to the paths,
+// and also converts the path separators on Windows.
+func prefixDir(paths []string, prefix string) []string {
+	prefixed := []string{}
+	for _, path := range paths {
+		osPath := strings.Join(strings.Split(filepath.Join(prefix, path), "/"), sep)
+
+		prefixed = append(prefixed, osPath)
+	}
+	return prefixed
+}
+
+// prefixImp appends the prefix to the import paths.
+func prefixImp(paths []string, prefix string) []string {
+	prefixed := []string{}
+	for _, path := range paths {
+		if path != "" {
+			prefixed = append(prefixed, prefix+"/"+path)
+		} else {
+			prefixed = append(prefixed, path)
+		}
+	}
+	return prefixed
+}
+
+var QueryTestDetails = []details{
+	{"/root/a/a", "a/a", true},
+	{"/root/b/a", "b/a", true},
+	{"/root/a", "a", true},
+	{"/root/ab", "ab", true},
+	{"/root/a/b", "a/b", false},
+	{"/long path/ab/ab", "ab/ab", true},
+	{"/long/path/ab/a.b", "ab/a.b", true},
+	{"/c-c/c.c", "c-c/c.c", false},
+	{"/c-c/c.c/c.c", "c-c/c.c/c.c", true},
+	{"/a/b/c", "a/b/c", true},
+	{"./d/d", "d/d", false},
+}
+
+var QueryImportsTests = []struct {
+	query string
+	out   []string
+}{
+	{"imports/a", []string{"a/a", "b/a", "a"}},
+	{"imports/a/a", []string{"a/a"}},
+	{"imports/b", []string{"a/b"}},
+	{"imports/ab", []string{"ab", "ab/ab"}},
+	{"imports/a.b", []string{"ab/a.b"}},
+	{"imports/c", []string{"a/b/c"}},
+	{"imports/c-c/c.c", []string{"c-c/c.c"}},
+	{"imports/c.c", []string{"c-c/c.c/c.c"}},
+}
+
+var QueryDirTests = []struct {
+	query string
+	out   []string
+}{
+	{"dirs/a", []string{"/root/a/a", "/root/b/a", "/root/a"}},
+	{"dirs/a/a", []string{"/root/a/a"}},
+	{"dirs/b", []string{"/root/a/b"}},
+	{"dirs/ab", []string{"/root/ab", "/long path/ab/ab"}},
+	{"dirs/a.b", []string{"/long/path/ab/a.b"}},
+	{"dirs/c", []string{"/a/b/c"}},
+	{"dirs/c-c/c.c", []string{"/c-c/c.c"}},
+	{"dirs/c.c", []string{"/c-c/c.c/c.c"}},
+}
+
+func TestQueryImports(t *testing.T) {
+	dirs := index{
+		index: QueryTestDetails,
+	}
+
+	for _, test := range QueryImportsTests {
+		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
+		if err != nil {
+			t.Errorf("GET %q failed", test.query)
+		}
+
+		rec := httptest.NewRecorder()
+		dirs.ServeMux().ServeHTTP(rec, req)
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, test.out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, test.out)
+		}
+	}
+}
+
+func TestQueryDirs(t *testing.T) {
+	dirs := index{
+		index: QueryTestDetails,
+	}
+
+	for _, test := range QueryDirTests {
+		query := test.query
+
+		if runtime.GOOS == "windows" {
+			query = strings.Join(strings.Split(query, "/"), sep)
+		}
+
+		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
+		if err != nil {
+			t.Errorf("GET %q failed", test.query)
+		}
+
+		rec := httptest.NewRecorder()
+		dirs.ServeMux().ServeHTTP(rec, req)
+
+		out := prefixDir(test.out, "")
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, out)
+		}
+	}
 }
 
 var IndexerImportsTests = []struct {
 	query string
 	out   []string
 }{
-	{"imports/fmt", []string{"fmt"}},
-	{"imports/math/rand", []string{"math/rand"}},
-	{"imports/rand", []string{"crypto/rand", "math/rand"}},
-	{"imports/template", []string{"html/template", "text/template"}},
+	{"imports/a", []string{"a"}},
+	{"imports/c", []string{"a/b/c"}},
+	{"imports/d", []string{"a/b/c/d", "d", "d/d"}},
+	{"imports/bb", []string{"aa/bb"}},
+	{"imports/c-c", []string{"c-c"}},
+	{"imports/c.c", []string{"c-c/c.c"}},
 }
 
 func TestIndexerImports(t *testing.T) {
-	dirs := index{}
-	dirs.Roots([]string{runtime.GOROOT()})
-	dirs.Index()
+	imps := index{}
+	imps.Roots([]string{"testdata"})
+	imps.Index()
 
 	for _, test := range IndexerImportsTests {
 		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
@@ -49,46 +174,32 @@ func TestIndexerImports(t *testing.T) {
 		}
 
 		rec := httptest.NewRecorder()
-		dirs.ServeHTTP(rec, req)
+		imps.ServeMux().ServeHTTP(rec, req)
 
-		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, test.out) != true {
-			t.Errorf("%q: got %q, want %q", test.query, actual, test.out)
+		out := prefixImp(test.out, packagePrefix)
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, out)
 		}
 	}
-}
-
-func compareDirs(actual, out []string) bool {
-	if len(actual) != len(out) {
-		return false
-	}
-
-	sep := string(os.PathSeparator)
-
-	for i, _ := range actual {
-		if !strings.HasSuffix(actual[i],
-			strings.Join(strings.Split(out[i], "/"), sep)) {
-
-			return false
-		}
-	}
-	return true
 }
 
 var IndexerDirTests = []struct {
 	query string
 	out   []string
 }{
-	{"dirs/fmt", []string{"fmt"}},
-	{"dirs/math/rand", []string{"math/rand"}},
-	{"dirs/cmd/gofmt", []string{"cmd/gofmt"}},
-	{"dirs/rand", []string{"crypto/rand", "math/rand"}},
-	{"dirs/template", []string{"html/template", "text/template"}},
+	{"a", []string{"/a"}},
+	{"c", []string{"/a/b/c"}},
+	{"d", []string{"/a/b/c/d", "/d", "/d/d"}},
+	{"bb", []string{"/aa/bb"}},
+	{"c-c", []string{"/c-c"}},
+	{"c.c", []string{"/c-c/c.c"}},
 }
 
 func TestIndexerDirs(t *testing.T) {
-	dirs := index{}
-	dirs.Roots([]string{runtime.GOROOT()})
-	dirs.Index()
+	imps := index{}
+	imps.Roots([]string{"testdata"})
+	imps.Index()
 
 	for _, test := range IndexerDirTests {
 		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
@@ -97,10 +208,12 @@ func TestIndexerDirs(t *testing.T) {
 		}
 
 		rec := httptest.NewRecorder()
-		dirs.ServeHTTP(rec, req)
+		imps.ServeMux().ServeHTTP(rec, req)
 
-		if actual := slice(rec.Body.String()); compareDirs(actual, test.out) != true {
-			t.Errorf("%q: got %q, want %q", test.query, actual, test.out)
+		out := prefixDir(test.out, dirPrefix)
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, out)
 		}
 	}
 }
@@ -109,18 +222,19 @@ var ExclusionsTests = []struct {
 	query string
 	out   []string
 }{
-	{"imports/math/rand", []string{""}},
-	{"imports/rand", []string{"crypto/rand"}},
-	{"imports/io", []string{""}},
-	{"imports/os", []string{"os"}},
-	{"imports/os/exec", []string{""}},
+	{"imports/a", []string{"a"}},
+	{"imports/c", []string{""}},
+	{"imports/d", []string{"d", "d/d"}},
+	{"imports/bb", []string{"aa/bb"}},
+	{"imports/c-c", []string{"c-c"}},
+	{"imports/c.c", []string{"c-c/c.c"}},
 }
 
 func TestExclusions(t *testing.T) {
-	dirs := index{}
-	dirs.Roots([]string{runtime.GOROOT()})
-	dirs.Exclusions(strings.NewReader(`io math exec`))
-	dirs.Index()
+	imps := index{}
+	imps.Roots([]string{"testdata"})
+	imps.Exclusions(strings.NewReader(`b c`))
+	imps.Index()
 
 	for _, test := range ExclusionsTests {
 		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
@@ -129,21 +243,23 @@ func TestExclusions(t *testing.T) {
 		}
 
 		rec := httptest.NewRecorder()
-		dirs.ServeHTTP(rec, req)
+		imps.ServeMux().ServeHTTP(rec, req)
 
-		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, test.out) != true {
-			t.Errorf("%q: got %q, want %q", test.query, actual, test.out)
+		out := prefixImp(test.out, packagePrefix)
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, out)
 		}
 	}
 }
 
-func TestDuplicateDirs(t *testing.T) {
-	dirs := index{}
-	dirs.Roots([]string{
-		runtime.GOROOT(),
-		runtime.GOROOT(),
+func TestDuplicateRoots(t *testing.T) {
+	imps := index{}
+	imps.Roots([]string{
+		"testdata",
+		"testdata",
 	})
-	dirs.Index()
+	imps.Index()
 
 	for _, test := range IndexerImportsTests {
 		req, err := http.NewRequest("GET", hostPrefix+test.query, nil)
@@ -152,10 +268,12 @@ func TestDuplicateDirs(t *testing.T) {
 		}
 
 		rec := httptest.NewRecorder()
-		dirs.ServeHTTP(rec, req)
+		imps.ServeMux().ServeHTTP(rec, req)
 
-		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, test.out) != true {
-			t.Errorf("%q: got %q, want %q", test.query, actual, test.out)
+		out := prefixImp(test.out, packagePrefix)
+
+		if actual := slice(rec.Body.String()); reflect.DeepEqual(actual, out) != true {
+			t.Errorf("%q: got %q, want %q", test.query, actual, out)
 		}
 	}
 }
